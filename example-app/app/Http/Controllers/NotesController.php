c0cb0be66176e7f056a9569b83b6fa4ffdd13ff2
Aditya Mishra <mishra.aditya1352@gmail.com>
2023-10-14 18:08:18 +0530

    created the createnote route

---
+++ example-app/app/Http/Controllers/NotesController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class NotesController extends Controller
+{
+    public function index(){
+        return view('notes.index');
+    }
+    public function create(){
+        return view('notes.create');
+    }
+}
